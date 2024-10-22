a25a77c22fbe095129834fe600b1066c09c84b96
samuelfsd <samuel.estudo778@gmail.com>
2024-10-22 07:53:33 -0300

    feat: view search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { CardWrapper } from "./core/presentation/components/Card";
+import { Search } from "./core/presentation/components/Search";
 
 export function App() {
   return (
     <>
+      <Search />
       <CardWrapper title="Ler livro" />
     </>
   )
