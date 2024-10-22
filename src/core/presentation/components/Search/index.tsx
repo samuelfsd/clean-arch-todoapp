9dc7dbc207d88483c6330a1561319b242bc36519
samuelfsd <samuel.estudo778@gmail.com>
2024-10-22 07:51:49 -0300

    feat: add search component

---
+++ src/core/presentation/components/Search/index.tsx
@@ -0,0 +1,22 @@
+import { MagnifyingGlassIcon, PlusIcon } from "@radix-ui/react-icons";
+import { Box, Button, Container, Flex, TextField } from "@radix-ui/themes";
+
+export function Search() {
+  return (
+    <Container size="4" my="6">
+      <Flex align="center" justify="center" gap="3" >
+        <Box flexGrow="1" maxWidth="800px">
+          <TextField.Root  size="3" placeholder="Pesquise aqui…">
+            <TextField.Slot>
+              <MagnifyingGlassIcon height="16" width="16" />
+            </TextField.Slot>
+          </TextField.Root>
+        </Box>
+
+        <Button size="3" color="green">
+            <PlusIcon /> Adicionar
+        </Button>
+      </Flex>
+    </Container>
+  )
+}
