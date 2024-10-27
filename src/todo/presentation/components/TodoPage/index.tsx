3c40420757e725c609ff503ef93a17d93a608e6c
samuelfsd <samuel.estudo778@gmail.com>
2024-10-27 15:53:42 -0300

    fix: add space in TodoCard

--- src/todo/presentation/components/TodoPage/index.tsx
+++ src/todo/presentation/components/TodoPage/index.tsx
@@ -1,11 +1,21 @@
+import { todos } from '../../../data/base';
 import { TodoCard } from '../TodoCard';
 import { TodoSearch } from '../TodoSearch';
+
+import styles from './styles.module.css';
 
 export function TodoPage() {
   return (
     <>
       <TodoSearch />
-      <TodoCard title="Ler um livro" />
+
+      {todos.map((todo) => {
+        return (
+          <section className={styles.section} key={todo.id}>
+            <TodoCard title={todo.title} />
+          </section>
+        );
+      })}
     </>
   );
 }
