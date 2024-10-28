08c3098927386c2fc2d748b9166b353995d2488b
samuelfsd <samuel.estudo778@gmail.com>
2024-10-28 20:17:26 -0300

    feat: add main layer

--- src/todo/main/makeTodo.tsx
+++ src/todo/main/makeTodo.tsx
@@ -1,1 +1,14 @@
-export function MakeTodo() {}
+import type { IGetTodosContract } from "../data/contracts/getTodosContract";
+import { GetTodosService } from "../data/services/getTodosService";
+
+export type MakeTodoType = IGetTodosContract;
+
+export function MakeTodo(): MakeTodoType {
+  const getTodos = new GetTodosService()
+
+  return {
+    getTodos() {
+      return getTodos.getTodos()
+    }
+  }
+}
