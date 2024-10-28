cee04360228582019042eecb7f2a64ea0f5dc9cf
samuelfsd <samuel.estudo778@gmail.com>
2024-10-28 20:17:59 -0300

    lint: use biome

--- src/todo/main/makeTodo.tsx
+++ src/todo/main/makeTodo.tsx
@@ -1,14 +1,14 @@
-import type { IGetTodosContract } from "../data/contracts/getTodosContract";
-import { GetTodosService } from "../data/services/getTodosService";
+import type { IGetTodosContract } from '../data/contracts/getTodosContract';
+import { GetTodosService } from '../data/services/getTodosService';
 
 export type MakeTodoType = IGetTodosContract;
 
 export function MakeTodo(): MakeTodoType {
-  const getTodos = new GetTodosService()
+  const getTodos = new GetTodosService();
 
   return {
     getTodos() {
-      return getTodos.getTodos()
-    }
-  }
+      return getTodos.getTodos();
+    },
+  };
 }
