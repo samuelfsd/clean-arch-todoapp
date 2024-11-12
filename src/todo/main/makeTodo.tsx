b2a12033e0ccb5ae735784dd18a55e7c89b576da
samuelfsd <samuel.estudo778@gmail.com>
2024-11-11 23:31:07 -0300

    feat: adiciona deletar uma tarefa na camada principal

--- src/todo/main/makeTodo.tsx
+++ src/todo/main/makeTodo.tsx
@@ -1,14 +1,21 @@
+import type { IDeleteTodoById } from '../data/contracts/deleteTodoByIdContract';
 import type { IGetTodosContract } from '../data/contracts/getTodosContract';
+import { DeleteTodoByIdService } from '../data/services/deleteTodoByIdService';
 import { GetTodosService } from '../data/services/getTodosService';
 
-export type MakeTodoProvider = IGetTodosContract;
+export type MakeTodoProvider = IGetTodosContract & IDeleteTodoById;
 
 export function makeTodo(): MakeTodoProvider {
   const getTodos = new GetTodosService();
 
+  const deleteTodoById = new DeleteTodoByIdService();
+
   return {
     getTodos() {
       return getTodos.getTodos();
+    },
+    deleteTodoById(id) {
+      return deleteTodoById.deleteTodoById(id);
     },
   };
 }
