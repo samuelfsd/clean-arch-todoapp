d14bdd7d4052cb762b224a4281cb6ec098afc878
samuelfsd <samuel.estudo778@gmail.com>
2024-12-10 22:40:45 -0300

    feat: adiciona função para adicionar tarefa na camada principal

--- src/todo/main/makeTodo.tsx
+++ src/todo/main/makeTodo.tsx
@@ -1,14 +1,16 @@
+import { IAddTodoContract } from '../data/contracts/addTodoContract';
 import type { IDeleteTodoById } from '../data/contracts/deleteTodoByIdContract';
 import type { IGetTodosContract } from '../data/contracts/getTodosContract';
+import { AddTodoService } from '../data/services/addTodoService';
 import { DeleteTodoByIdService } from '../data/services/deleteTodoByIdService';
 import { GetTodosService } from '../data/services/getTodosService';
 
-export type MakeTodoProvider = IGetTodosContract & IDeleteTodoById;
+export type MakeTodoProvider = IGetTodosContract & IDeleteTodoById & IAddTodoContract;
 
 export function makeTodo(): MakeTodoProvider {
   const getTodos = new GetTodosService();
-
   const deleteTodoById = new DeleteTodoByIdService();
+  const addTodo = new AddTodoService();
 
   return {
     getTodos() {
@@ -16,6 +18,9 @@ export function makeTodo(): MakeTodoProvider {
     },
     deleteTodoById(id) {
       return deleteTodoById.deleteTodoById(id);
+    },
+    addTodo(payload) {
+      return addTodo.addTodo(payload);
     },
   };
 }
