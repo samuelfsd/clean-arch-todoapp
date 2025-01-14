c5a404591f4636e2553748a3e9cc0ca86cbd2c12
samuelfsd <samuel.estudo778@gmail.com>
2025-01-14 17:10:28 -0300

    feat: add sonner

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Toaster } from 'sonner';
 import { TodoPageFactory } from './todo/presentation/components/TodoPage';
 
 export function App() {
   return (
     <>
       <TodoPageFactory />
+
+      <Toaster position="top-right" richColors />
     </>
   );
 }
